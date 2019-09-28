23c12d179c7d94170f8cd47d7cd5981daa259087
supergrecko <me@supergrecko.com>
2019-09-28 10:39:28 +0200

    patch(0.0.0): Add Antlr files

---
+++ src/main/antlr4/com/supergrecko/diamond/codegen/DiamondBaseListener.java
@@ -0,0 +1,51 @@
+// Generated from /mnt/d/Projects/Kotlin/diamond/src/main/antlr4/com/supergrecko/diamond/codegen/DiamondLexer.g4 by ANTLR 4.7.2
+package com.supergrecko.diamond.codegen;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link DiamondListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class DiamondBaseListener implements DiamondListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterR(DiamondParser.RContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitR(DiamondParser.RContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ src/main/antlr4/com/supergrecko/diamond/codegen/DiamondListener.java
@@ -0,0 +1,20 @@
+// Generated from /mnt/d/Projects/Kotlin/diamond/src/main/antlr4/com/supergrecko/diamond/codegen/DiamondLexer.g4 by ANTLR 4.7.2
+package com.supergrecko.diamond.codegen;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link DiamondParser}.
+ */
+public interface DiamondListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link DiamondParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(DiamondParser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link DiamondParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(DiamondParser.RContext ctx);
+}
