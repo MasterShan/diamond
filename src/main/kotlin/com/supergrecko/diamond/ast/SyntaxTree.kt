db4f7e1789113bc1408879846919f1744061e1c0
supergrecko <me@supergrecko.com>
2019-10-06 19:14:49 +0200

    refactor(0.0.0): Continue work on AST

--- src/main/kotlin/com/supergrecko/diamond/ast/SyntaxTree.kt
+++ src/main/kotlin/com/supergrecko/diamond/ast/SyntaxTree.kt
@@ -1,6 +1,9 @@
 package com.supergrecko.diamond.ast
 
-sealed class SyntaxTree
+sealed class SyntaxTree {
+    // Variable for AST Printing
+    private val name = SyntaxTree::class.java.name
+}
 
 data class IntLiteral(val value: Int) : SyntaxTree()
 data class DoubleLiteral(val value:Double) : SyntaxTree()
@@ -16,6 +19,25 @@ data class AdditiveExpression(val left: SyntaxTree, val operator: SyntaxTree, va
 data class BinaryExpression(val left: SyntaxTree, val operator: SyntaxTree, val right: SyntaxTree) : SyntaxTree()
 data class InfixExpression(val left: SyntaxTree, val operator: SyntaxTree, val right: SyntaxTree) : SyntaxTree()
 data class EqualityExpression(val left: SyntaxTree, val operator: SyntaxTree, val right: SyntaxTree) : SyntaxTree()
+data class FunctionExpression(val types: List<SyntaxTree>, val parameters: List<SyntaxTree>, val body: List<SyntaxTree>, val returns: SyntaxTree) : SyntaxTree()
+data class IfExpression(val condition: SyntaxTree, val primary: List<SyntaxTree>, val secondary: SyntaxTree) : SyntaxTree()
+data class WhenExpression(val condition: SyntaxTree, val branches: List<SyntaxTree>, val fallback: SyntaxTree) : SyntaxTree()
 
 data class NamespaceDeclaration(val name: String) : SyntaxTree()
 data class EntryDeclaration(val body: List<SyntaxTree>) : SyntaxTree()
+data class FunctionDeclaration(val name: String, val types: List<SyntaxTree>, val parameters: List<SyntaxTree>, val body: List<SyntaxTree>, val returns: SyntaxTree) : SyntaxTree()
+data class StructDeclaration(val name: String, val types: List<SyntaxTree>, val contracts: List<SyntaxTree>, val body: List<SyntaxTree>) : SyntaxTree()
+data class ContractDeclaration(val name: String, val types: List<SyntaxTree>, val body: List<SyntaxTree>) : SyntaxTree()
+
+data class MutStatement(val name: String, val type: SyntaxTree, val value: SyntaxTree) : SyntaxTree()
+data class ConstStatement(val name: String, val type: SyntaxTree, val value: SyntaxTree) : SyntaxTree()
+data class ExpressionStatement(val expression: SyntaxTree) : SyntaxTree()
+data class LoopStatement(val condition: SyntaxTree, val body: List<SyntaxTree>) : SyntaxTree()
+data class ReturnStatement(val value: SyntaxTree) : SyntaxTree()
+data class YieldStatement(val value: SyntaxTree) : SyntaxTree()
+
+object ThisExpression : SyntaxTree()
+object BreakStatement : SyntaxTree()
+object ContinueStatement : SyntaxTree()
+
+data class WhenBranch(val condition: SyntaxTree, val event: List<SyntaxTree>) : SyntaxTree()
